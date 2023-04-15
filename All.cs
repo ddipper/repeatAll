9023553407a939ac5a29c1e6a21042476eb334e9
Dmitry <dimonbyt@gmail.com>
2023-04-15 20:50:45 +0300

    Add project files.

---
+++ All.cs
@@ -0,0 +1,35 @@
+﻿/*************************       VARIABLE        *************************/
+//string Name = "Bob"; //определение и инициализация переменной
+//const string NAME = "Tom"; //создание константы, должна быть инициализирована при определении, после значение не может быть изменено
+
+/*************************       LITERAL        *************************/
+/*Литералы представляют неизменяемые значения (иногда их еще называют константами). Литералы можно передавать переменным в качестве значения.
+Литералы бывают логическими, целочисленными, вещественными, символьными и строчными. И отдельный литерал представляет ключевое слово null.*/
+//Literal.cs
+//логические литералы
+//Console.WriteLine(true);
+//Console.WriteLine(false);
+/*---   Целочисленные литералы    ---*/
+//целые числа в десятичной форме используются как и в повседневной жизни
+/*Console.WriteLine(-10);
+Console.WriteLine(5);
+//числа в двоичной форме предваряются символами 0b, после которых идет набор из нулей и единиц
+Console.WriteLine(0b11);     // 3
+Console.WriteLine(0b1011);   // 11
+//числа в шестнадцатеричной форме применяются символы 0x, после которых идет набор символов от 0 до 9 и от A до F
+Console.WriteLine(0x0A);     // 10
+Console.WriteLine(0xFF);     // 255
+*/
+
+/*Console.WriteLine("Your name: ");
+string? name = Console.ReadLine();
+Console.WriteLine("You age: ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"Name: {name}, age: {age}");*/
+
+
+
+
+/*Console.WriteLine("Enter your name: ");
+var name = Console.ReadLine();
+Console.WriteLine($"Your name: {name}");*/
