429aeb47579b57149a4baeda3ec05c2ab2f0e349
Dmitry <dimonbyt@gmail.com>
2023-04-16 12:24:44 +0300

    add

--- All.cs
+++ All.cs
@@ -27,6 +27,126 @@ Console.WriteLine("You age: ");
 int age = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine($"Name: {name}, age: {age}");*/
 
+/*int a = 10;
+int b = 2;
+int c = a ---b;
+Console.WriteLine(c);*/
+
+/*************************       LOGIC OPERATION        *************************/
+/*int x1 = 2; //010     &
+int y1 = 5;//101
+Console.WriteLine(x1 & y1); // выведет 0
+
+int x2 = 4; //100
+int y2 = 5; //101
+Console.WriteLine(x2 & y2); // выведет 4*/
+
+/*int x1 = 2; //010     |
+int y1 = 5;//101
+Console.WriteLine(x1|y1); // выведет 7 - 111
+int x2 = 4; //100
+int y2 = 5;//101
+Console.WriteLine(x2 | y2); // выведет 5 - 101*/
+
+/*int x = 45; // Значение, которое надо зашифровать - в двоичной форме 101101    ^
+int key = 102; //Пусть это будет ключ - в двоичной форме 1100110
+int encrypt = x ^ key; //Результатом будет число 1001011 или 75
+Console.WriteLine($"Зашифрованное число: {encrypt}") ; 
+int decrypt = encrypt ^ key; // Результатом будет исходное число 45
+Console.WriteLine($"Расшифрованное число: {decrypt}");*/
+
+/*int x = 12;                 // 00001100       ~
+Console.WriteLine(~x);      // 11110011   или -13*/
+
+/*int num1 = 6;
+int num2 = 10;
+if(num1 < num2)
+    Console.WriteLine($"{num1} меньше {num2}");
+// -----------------------------     Ternar construction     -----------------------------
+// int c = a > b ? a - b : a + b;
+// int c = условие ? (если true) : (если false);
+int num3 = num1 > num2 ? num1 + num2 : num2 - num1;
+Console.WriteLine(num3);*/
+
+/*int[] nums = {1, 2, 3, 4, 5};
+Console.WriteLine(nums[^1]); //получение последнего элемента массива*/
+
+/*int[,] mass1 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
+Console.WriteLine(mass1.GetUpperBound(0) + 1);  //2
+Console.WriteLine(mass1.GetUpperBound(1) + 1);  //3
+for (int i = 0; i < mass1.GetUpperBound(0) + 1; i++)
+{
+    for (int j = 0; j < mass1.Length/(mass1.GetUpperBound(0)+1); j++)
+    {
+        Console.Write($"{mass1[i, j]} \t");
+    }
+    Console.WriteLine();
+}*/
+
+// -----------------------------     Array invertion     -----------------------------
+/*int[] numbers = { -4, -3, 2, 3, 1, -5 };
+int n = numbers.Length;
+int k = n / 2;
+int temp = 0;
+for (int i = 0; i < k; i++)
+{
+    temp = numbers[i];
+    numbers[i] = numbers[n-i-1];
+    numbers[n-i-1] = temp;
+}
+foreach(int i in numbers)
+    Console.WriteLine($"{i}\t");*/
+
+// -----------------------------     Array sort     -----------------------------
+/*int[] nums = { 54, 7, -41, 2, 4, 2, 89, 33, -5, 12 };
+int temp;
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    for (int j = i + 1; j < nums.Length; j++)
+    {
+        if (nums[i] > nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
+Console.WriteLine("sort array: ");
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.WriteLine(nums[i]);
+}*/
+
+/*************************       METHODS        *************************/
+/*SayHello();
+SayHelloRu();
+void SayHello()
+{
+    Console.WriteLine("Hello bro!");
+}
+void SayHelloRu() => Console.WriteLine("Привет"); //Сокращенная запись, если метод имеет только одну конструкцию*/
+
+/*PrintMessage("Hello");
+string ms = Console.ReadLine();
+PrintMessage($"{ms}");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int number2 = Convert.ToInt32(Console.ReadLine());
+Sum(number1, number2);
+void PrintMessage(string message)
+{
+    Console.WriteLine($"not - {message}");
+}
+void Sum(int a, int b) => Console.WriteLine($"{a} + {b} = {a + b}");*/
+
+/*string GetMessage()
+{
+    return "Hello";
+}
+Console.WriteLine(GetMessage());
+string GetMessageRu() => "Привет";
+Console.WriteLine(GetMessageRu());*/
+
 
 
 
